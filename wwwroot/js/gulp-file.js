511434d793bbdafb580939b1b27b6211b5816745
OHOTEENI <nketiaernest18@gmail.com>
2021-07-14 15:06:56 -0700

    meme

---
+++ wwwroot/js/gulp-file.js
@@ -0,0 +1,12 @@
+﻿var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+gulp.task('build-css', function () {
+    return gulp
+        .src('../sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('../css'));
+});
+
+//Default Task
+gulp.task('default', ['build-css']);
